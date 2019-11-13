0edc9622c9b291f4e37827488b6a83496511df09
Błażej Kasperski <kasperski.blazej@gmail.com>
2019-11-13 11:52:48 +0100

    Updates

---
+++ precache-manifest.5b1834ae18e20b9d5c63e0fd69bf6c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8f5d884792e3e2b9383d4790e2cf8f",
+    "url": "/b2b/index.html"
+  },
+  {
+    "revision": "4c5d60640f7d46d2b48b",
+    "url": "/b2b/static/css/main.8a42558d.chunk.css"
+  },
+  {
+    "revision": "c0aee0c967314f505211",
+    "url": "/b2b/static/js/2.a436cd94.chunk.js"
+  },
+  {
+    "revision": "4c5d60640f7d46d2b48b",
+    "url": "/b2b/static/js/main.f65ad6f7.chunk.js"
+  },
+  {
+    "revision": "19696fecc02bd98d4a8b",
+    "url": "/b2b/static/js/runtime-main.cbeba00b.js"
+  }
+]);
